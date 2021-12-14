baa741db6798126637b8c85ffcb5617b6741254f
Nick Hafkemeyer <mail@nhafkemeyer.com>
2021-12-14 08:56:17 +0100

    feat: change env variables

--- client-vue-app/src/main.js
+++ client-vue-app/src/main.js
@@ -7,9 +7,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    apiKey: process.env._FIREBASE_API_KEY,
     authDomain: `${import.meta.env.VITE_GCLOUD_PROJECT}.firebaseapp.com`,
-    projectId: import.meta.env.VITE_GCLOUD_PROJECT,
+    projectId: process.env.VITE_GCLOUD_PROJECT,
     storageBucket: `${import.meta.env.VITE_GCLOUD_PROJECT}.appspot.com`,
     messagingSenderId: import.meta.env.VITE_PROJECT_NUMBER,
     appId: "1:72095501801:web:d256ee1fd1eeff7f91cf78"
